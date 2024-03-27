9b2f4e4542536f4eef9a5859cfb9e4048756389b
sujan rokka <60314367+sujanrokka@users.noreply.github.com>
2024-03-27 19:41:38 +0545

    update and delete added

--- crudapp/urls.py
+++ crudapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import Home,Add_Student
+from .views import Home,Add_Student,Delete_Student,Edit_Student
 urlpatterns = [
     path('',Home.as_view(),name='home'),
     path('add-student/',Add_Student.as_view(),name='add-student'),
+    path('delete-student/',Delete_Student.as_view(),name='delete-student'),
+    path('edit-student/<int:id>',Edit_Student.as_view(),name='edit-student'),
+    
 ]
+
     
