d2a0fac8941028ab738d44ad300d23ea635c6072
sujan rokka <60314367+sujanrokka@users.noreply.github.com>
2024-08-29 15:27:55 +0545

    rich text editor added

--- crudapp/models.py
+++ crudapp/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+
 
 class Student(models.Model):
     name=models.CharField(max_length=60)
     roll=models.IntegerField()
-    city=models.CharField(max_length=50)
+    # city=models.CharField(max_length=50)
+    city = RichTextField(blank= True, null = True)
     
